30e84960a6b3978baf63f2d0cf387b8553bf7c92
Mark <kiwiszijncool@gmail.com>
2020-01-11 17:50:17 +0100

    Adding custom plugins #1

---
+++ plugins/roenstaak/command/teleportBook/teleportBook/build.plugin.kts
@@ -0,0 +1,13 @@
+package roenstaak.command.teleportBook.teleportBook
+
+import api.bootstrap.plugin
+
+plugin {
+    name = "Teleport book"
+    description =
+        """
+        Im lazy
+        """
+    version = "1.0"
+    authors += "Roenstaak"
+}
---
+++ plugins/roenstaak/fishing/build.plugin.kts
@@ -0,0 +1,13 @@
+package roenstaak.fishing
+
+import api.bootstrap.plugin
+
+plugin {
+    name = "Fishing additions"
+    description =
+        """
+        Im lazy
+        """
+    version = "1.0"
+    authors += "Roenstaak"
+}
